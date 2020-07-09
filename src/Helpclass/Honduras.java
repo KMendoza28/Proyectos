d99cccab6c8c8ff6dda6ada58892a017175971fc
carlos <carlos@KarenFlores>
2020-07-09 17:57:07 -0600

    Encapsulamiento en Programación Orientada a Objetos

---
+++ src/Helpclass/Honduras.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helpclass;
+
+/**
+ *
+ * @author carlos
+ */
+public class Honduras extends Pais {
+
+    public String getPais;
+    public String getPresidente;
+
+    @Override
+    public String getPais() {
+        return "Honduras"; 
+    }
+
+    @Override
+    public String getPresidente() {
+     return "Juan Orlando Hernandez";
+    }
+    
+    
+    
+}
---
+++ src/abstraccion1/Abstraccion1.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstraccion1;
+
+
+
+import helpclass.CostaRica;
+import helpclass.ElSalvador;
+import helpclass.Honduras;
+
+/**
+ *
+ * @author carlos
+ */
+public class Abstraccion1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Honduras hn=new Honduras();
+        CostaRica cr=new CostaRica();
+        ElSalvador esv=new ElSalvador ();
+        
+        System.out.println(""+hn.getPais()+ ":" +hn.getPresidente());
+        System.out.println(""+cr.getPais()+ ":" +cr.getPresidente());
+        System.out.println(""+esv.getPais()+ ":" +esv.getPresidente());
+        
+        
+       
+        
+        
+        
+      
+        
+    }
+    
+}
