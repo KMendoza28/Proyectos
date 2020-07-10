a9e4891687eae1b9834c0be47df4c13520534fbc
carlos <carlos@KarenFlores>
2020-07-09 18:23:12 -0600

    Abstaccion Programación Orientada a Objetos

---
+++ Abstraccion/src/helpclass/CostaRica.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helpclass;
+
+/**
+ *
+ * @author carlos
+ */
+public class CostaRica extends Pais {
+
+    public String getPais;
+    public String getPresidente;
+   
+
+    @Override
+    public String getPais() {
+        return "CostaRica"; 
+    }
+
+    @Override
+    public String getPresidente() {
+     return "Carlos Alvarado Quesada";
+    }
+    
+    
+}
---
+++ Abstraccion/src/helpclass/Pais.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helpclass;
+
+/**
+ *
+ * @author carlos
+ */
+public abstract class Pais {
+    //Declaracion de metodo abstracto
+    abstract String getPais ();
+    abstract String getPresidente ();
+    
+    
+    
+}
